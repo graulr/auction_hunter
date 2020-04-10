f20e67955791137db8c829d54ba1fd99ad290d3f
graulr <graulr.xi@gmail.com>
2020-04-10 13:51:51 -0400

    Refactor

--- script/auction_hunter.py
+++ script/auction_hunter.py
@@ -5,14 +5,30 @@ import os
 import requests
 import time
 import sys
+import enum
 
 from bs4 import BeautifulSoup
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-RED = 'red'
-YELLOW = 'yellow'
-GREEN = 'green'
+
+class Modes(enum.Enum):
+    INVENTORY = 'inventory'
+    PRICE = 'price'
+    PLAYER = 'player'
+
+
+class Results(enum.Enum):
+    CONTINUE_SEARCHING = 1
+    COMPLETED = 2
+    FAILURE = 3
+
+
+class Colors(enum.Enum):
+    RED = 1
+    YELLOW = 2
+    GREEN = 3
+
 
 MAX_RETRIES = 5
 MAX_LINE_LENGTH = 57
@@ -42,13 +58,15 @@ AUCTION_HUNTER_DIRECTORY_PATH = PATH_TO_SCRIPT[0:len(PATH_TO_SCRIPT)-6]
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+global_sleep_time = 5
+
 
 # This script fetches the supplied ffxiah url, checks the item value, & repeats util
 # the item value matches the users specification.  The user is then notified via email.
 def main():
     colorama.init()
-    print_and_log('\n-----================ auction_hunter ================-----', GREEN)
-    print_and_log('Type ctrl + c at any time to quit (cmd + c for mac).', YELLOW)
+    print_and_log('\n-----================ auction_hunter ================-----', Colors.GREEN)
+    print_and_log('Type ctrl + c at any time to quit (cmd + c for mac).', Colors.YELLOW)
 
     # Create the data folder if it does not exist
     create_folder('data')
@@ -62,43 +80,50 @@ def main():
     # Get the stored server id or solicit one and store it
     cookies = {'sid': get_server_id()}
 
-    # Get the stored sleep time or solicit one and store it
-    sleep_time = get_sleep_time()
+    # Set the global_sleep_time
+    set_sleep_time()
 
-    restart_options = {'should_restart': True}
-    while restart_options['should_restart']:
+    # Get the hunt mode
+    hunt_mode = get_hunt_mode()
 
-        # Get the ffxi url and item to check
-        url, item_name = get_ffxiah_url_and_item()
+    # Get the ffxi url and item to check
+    url, item_name = get_ffxiah_url_and_item()
 
-        # Setup a logging file for the item
-        setup_logging(item_name)
+    # Setup a logging file for the item
+    setup_logging(item_name)
+
+    continue_options = {'should_restart': True}
+    while continue_options.get('should_restart', False):
 
         # Get the config options from the user
         config_options = get_config_options(item_name)
 
-        restart_options['should_retry'] = True
-        while restart_options['should_retry']:
+        continue_options['should_retry'] = True
+        while continue_options.get('should_retry', False):
 
             # Start checking ffxiah and get any restart options afterwards
-            restart_options = check_ffxiah(cookies, url, item_name, sleep_time, config_options)
+            continue_options = check_ffxiah(cookies, url, item_name, hunt_mode, config_options)
 
 
-def check_ffxiah(cookies, url, item_name, sleep_time, config_options):
-    print_and_log('\n-----=============== Checking FFXIAH ===============-----', GREEN)
+def check_ffxiah(cookies, url, item_name, hunt_mode, config_options):
+    print_and_log('\n-----=============== Checking FFXIAH ===============-----', Colors.GREEN)
 
     # Process url parameters for the request
     base_url, url_params = parse_url(url)
 
     attempt = 0  # A count of attempts for logging
     consecutive_failures = 0  # A count of consecutive failures
+
+    log('url: %s' % base_url)
+    log('params: %s' % url_params)
+    log('cookies: %s' % cookies)
+
     while True:
+
+        attempt += 1
 
         # Fetch the page
         try:
-            log('url: %s' % base_url)
-            log('params: %s' % url_params)
-            log('cookies: %s' % cookies)
             response = requests.get(base_url, headers=HEADERS, cookies=cookies, params=url_params)
         except requests.ConnectionError as e:
             handle_and_log_error(consecutive_failures,
@@ -112,94 +137,118 @@ def check_ffxiah(cookies, url, item_name, sleep_time, config_options):
         # Parse the page
         soup = BeautifulSoup(response.text, 'lxml')
 
-        # Find the item count element
-        current_stock = soup.findAll('span', {'class': 'stock'})
-        if not len(current_stock):
-            handle_and_log_error(consecutive_failures,
-                                 'Current stock was not found on the page',
-                                 'Failed to find current stock %s consecutive times.' % redify(MAX_RETRIES))
-            if consecutive_failures == MAX_RETRIES:
-                return get_should_retry()
-            consecutive_failures += 1
-            continue
+        # Inventory mode
+        if hunt_mode == Modes.INVENTORY:
+            result = check_inventory(soup, item_name, url, config_options, consecutive_failures, attempt)
 
-        # Attempt to parse the current item count to an integer
-        total_count_in_stock = 0
-        try:
-            total_count_in_stock = int(current_stock[0].text)
-        except Exception as e:
-            handle_and_log_error(consecutive_failures,
-                                 'Could not parse stock count: \n%s' % e,
-                                 'Failed to parse stock count %s consecutive times.' % redify(MAX_RETRIES))
-            if consecutive_failures == MAX_RETRIES:
-                return get_should_retry()
-            consecutive_failures += 1
-            continue
+        # Price mode
+        elif hunt_mode == Modes.PRICE:
+            result = check_price(soup, item_name, url, config_options, consecutive_failures, attempt)
 
-        # Reset failure count since we've made it this far
-        consecutive_failures = 0
+        # Player mode
+        elif hunt_mode == Modes.PLAYER:
+            result = check_player(soup, item_name, url, config_options, consecutive_failures, attempt)
 
-        # Increment the attempts
-        attempt += 1
+        # Handle result
+        if result:
+            if result == Results.FAILURE:
+                consecutive_failures += 1
+                if consecutive_failures == MAX_RETRIES:
+                    return get_retry_options()
 
-        # Looking for 0 items on the AH
-        if config_options['is_count_down']:
-            print_and_log('#%s check for %s %s:' % (greenify(attempt), greenify('empty'), item_name))
-
-            # No items found
-            if is_stock_empty(total_count_in_stock):
-                print_and_log('Found 0 %s!' % item_name, color=GREEN, indent=True)
-                send_email(item_name, url, 'There are 0 %s up for sale' % item_name)
-                return get_should_restart()
-
-            # Items in stock
-            else:
-                print_and_log('Found %s %s' % (redify(total_count_in_stock), item_name), indent=True)
-
-                # Sleep before attempting to try again
-                sleep(sleep_time)
+            elif result == Results.CONTINUE_SEARCHING:
+                consecutive_failures = 0
                 continue
 
-        # Looking for a range on the AH
-        elif config_options['is_range']:
-            lower_bound = config_options['lower_bound']
-            upper_bound = config_options['upper_bound']
-            print_and_log('#%s check for %s within %s (%s - %s):' %
-                          (greenify(attempt), item_name, greenify('range'), lower_bound, upper_bound))
+            elif result == Results.COMPLETED:
+                return get_restart_options()
 
-            # Within range
-            if is_stock_within_range(total_count_in_stock, lower_bound, upper_bound):
-                print_and_log('Found %s %s! (range %s - %s)' % (total_count_in_stock, item_name,
-                                                                lower_bound, upper_bound), color=GREEN, indent=True)
-                send_email(item_name, url, 'There %s %s %s up for sale' % (get_is_or_are(total_count_in_stock),
-                                                                           total_count_in_stock, item_name))
-                return get_should_restart()
+        # We have no result, kill the loop
+        print_and_log('An error has occured, halting execution', Colors.RED)
+        log(result)
+        return {}
 
-            # Out of range
-            else:
-                print_and_log('Found %s %s' % (redify(total_count_in_stock), item_name), indent=True)
 
-                # Sleep before attempting to try again
-                sleep(sleep_time)
-                continue
+#######################################################################################################################
+#                                                      Inventory                                                      #
+#######################################################################################################################
+def check_inventory(soup, item_name, url, config_options, consecutive_failures, attempt):
+    # Find the item count element
+    current_stock = soup.findAll('span', {'class': 'stock'})
 
-        # Looking for at least 1 item on the AH
-        else:
-            print_and_log('#%s check for %s %s:' % (greenify(attempt), greenify('stocked'), item_name))
+    # Parse the string found into an integer
+    total_in_stock = get_total_in_stock(current_stock, consecutive_failures)
 
-            # If there are 0 in stock:
-            if is_stock_empty(total_count_in_stock):
-                # Sleep before attempting to try again
-                print_and_log('Found %s %s' % (redify(0), item_name), indent=True)
-                sleep(sleep_time)
-                continue
+    # Looking for 0 items
+    if config_options['is_count_down']:
+        return check_inventory_empty(total_in_stock, url, item_name, attempt)
 
-            # The item is in stock
-            else:
-                print_and_log('Found %s %s!' % (total_count_in_stock, item_name), color=GREEN, indent=True)
-                send_email(item_name, url, 'There %s %s %s up for sale' % (get_is_or_are(total_count_in_stock),
-                                                                           total_count_in_stock, item_name))
-                return get_should_restart()
+    # Looking for a range
+    if config_options['is_range']:
+        return check_inventory_range(total_in_stock, url, item_name, attempt, config_options)
+
+    # Looking for at least 1
+    return check_inventory_stocked(total_in_stock, url, item_name, attempt)
+
+
+def check_inventory_empty(total_in_stock, url, item_name, attempt):
+    print_and_log('#%s check for %s %s:' % (greenify(attempt), greenify('empty'), item_name))
+
+    # No items found
+    if total_in_stock == 0:
+        print_and_log('Found 0 %s!' % item_name, color=Colors.GREEN, indent=True)
+        send_email(item_name, url, 'There are 0 %s up for sale' % item_name)
+        return Results.COMPLETED
+
+    # Items in stock
+    else:
+        print_and_log('Found %s %s' % (redify(total_in_stock), item_name), indent=True)
+
+        # Sleep before attempting to try again
+        sleep(global_sleep_time)
+        return Results.CONTINUE_SEARCHING
+
+
+def check_inventory_range(total_in_stock, url, item_name, attempt, config_options):
+    lower_bound = config_options['lower_bound']
+    upper_bound = config_options['upper_bound']
+    print_and_log('#%s check for %s within %s (%s - %s):' %
+                  (greenify(attempt), item_name, greenify('range'), lower_bound, upper_bound))
+
+    # Within range
+    if is_stock_within_range(total_in_stock, lower_bound, upper_bound):
+        print_and_log('Found %s %s! (range %s - %s)' % (total_in_stock, item_name,
+                                                        lower_bound, upper_bound), color=Colors.GREEN, indent=True)
+        send_email(item_name, url, 'There %s %s %s up for sale' % (get_is_or_are(total_in_stock),
+                                                                   total_in_stock, item_name))
+        return Results.COMPLETED
+
+    # Out of range
+    else:
+        print_and_log('Found %s %s' % (redify(total_in_stock), item_name), indent=True)
+
+        # Sleep before attempting to try again
+        sleep(global_sleep_time)
+        return Results.CONTINUE_SEARCHING
+
+
+def check_inventory_stocked(total_in_stock, url, item_name, attempt):
+    print_and_log('#%s check for %s %s:' % (greenify(attempt), greenify('stocked'), item_name))
+
+    # If there are 0 in stock:
+    if is_stock_empty(total_in_stock):
+        print_and_log('Found %s %s' % (redify(0), item_name), indent=True)
+
+        # Sleep before attempting to try again
+        sleep(global_sleep_time)
+        return Results.CONTINUE_SEARCHING
+
+    # The item is in stock
+    else:
+        print_and_log('Found %s %s!' % (total_in_stock, item_name), color=Colors.GREEN, indent=True)
+        send_email(item_name, url, 'There %s %s %s up for sale' % (get_is_or_are(total_in_stock),
+                                                                   total_in_stock, item_name))
+    return Results.COMPLETED
 
 
 #######################################################################################################################
@@ -213,7 +262,7 @@ def get_int_user_input(message):
         try:
             answer = int(answer)
         except:
-            print_and_log('Expected an integer, received %s' % type(answer), YELLOW)
+            print_and_log('Expected an integer, received %s' % type(answer), Colors.YELLOW)
     return answer
 
 
@@ -254,7 +303,7 @@ def get_ffxiah_url_and_item():
             if query_params:
                 item_name += '-stack'
         except ValueError as e:
-            print_and_log(e, YELLOW)
+            print_and_log(e, Colors.YELLOW)
     return url, item_name
 
 
@@ -265,8 +314,8 @@ def get_config_options(item_name):
     print_and_log(message)
     script_type = get_option_user_input({'empty', 'stocked', 'range'},
                                         'Type %s, %s, or %s and press enter.' % (greenify('empty'),
-                                                                                   greenify('stocked'),
-                                                                                   greenify('range')))
+                                                                                 greenify('stocked'),
+                                                                                 greenify('range')))
     is_count_down = script_type == 'empty'
     is_range = script_type == 'range'
 
@@ -296,18 +345,16 @@ def get_boolean_input():
     return answer == 'y'
 
 
-def get_should_retry():
-    print_and_log('\nWould you like to resume the script and keep trying?', YELLOW)
-    restart_options = {'should_restart': False}
-    restart_options['should_retry'] = get_boolean_input()
-    return restart_options
-
-
-def get_should_restart():
+def get_restart_options():
     print_and_log('\nWould you like to run the script again?')
-    restart_options = {'should_retry': False}
-    restart_options['should_restart'] = get_boolean_input()
+    restart_options = {'should_restart': get_boolean_input()}
     return restart_options
+
+
+def get_retry_options():
+    print_and_log('\nWould you like to resume the script and keep trying?', Colors.YELLOW)
+    retry_options = {'should_retry': get_boolean_input()}
+    return retry_options
 
 
 def get_send_grid_key():
@@ -340,7 +387,17 @@ def get_server_id():
     return server_id
 
 
-def get_sleep_time():
+def get_hunt_mode():
+    message = line_breakify_message('Would you like this script to hunt based on inventory, price, or player?')
+    print_and_log(message)
+    hunt_mode = get_option_user_input({Modes.INVENTORY.value, Modes.PRICE.value, Modes.PLAYER.value},
+                                      'Type %s, %s, or %s and press enter.' % (greenify('inventory'),
+                                                                               greenify('price'),
+                                                                               greenify('player')))
+    return Modes[hunt_mode.upper()]
+
+
+def set_sleep_time():
     file_path = 'data/sleep_time.txt'
     sleep_time = get_file_data(file_path)
     if sleep_time is None:
@@ -350,10 +407,11 @@ def get_sleep_time():
         while sleep_time < 1:
             sleep_time = get_int_user_input(message)
             if sleep_time < 1:
-                print_and_log('Must supply an integer greater than 0.', YELLOW)
+                print_and_log('Must supply an integer greater than 0.', Colors.YELLOW)
 
         store_data(file_path, str(sleep_time))
-    return int(sleep_time)
+    global global_sleep_time
+    global_sleep_time = int(sleep_time)
 
 
 #######################################################################################################################
@@ -387,15 +445,15 @@ def print_and_log(message, color=None, indent=False):
 
 def print_failure_and_sleep(consecutive_failures):
     print_and_log('Re-attempting %s out of %s times after sleeping' %
-                  (consecutive_failures, MAX_RETRIES), YELLOW)
+                  (consecutive_failures, MAX_RETRIES), Colors.YELLOW)
     sleep(5)
 
 
 def handle_and_log_error(consecutive_failures, attempt_message, final_failure_message):
-    print_and_log(attempt_message, RED)
+    print_and_log(attempt_message, Colors.RED)
     if consecutive_failures == MAX_RETRIES:
         print_and_log(final_failure_message)
-        print_and_log('---------------------------------------------------------', RED)
+        print_and_log('---------------------------------------------------------', Colors.RED)
     if consecutive_failures != MAX_RETRIES:
         print_failure_and_sleep(consecutive_failures + 1)
 
@@ -472,11 +530,11 @@ def line_breakify_message(message, green_words=None):
 def format_message(message, color, indent):
     if indent:
         message = '   %s' % message
-    if color == RED:
+    if color == Colors.RED:
         message = redify(message)
-    elif color == YELLOW:
+    elif color == Colors.YELLOW:
         message = crayons.yellow(message)
-    elif color == GREEN:
+    elif color == Colors.GREEN:
         message = greenify(message)
     return message
 
@@ -548,12 +606,32 @@ def parse_url(url):
     return stack_split_list[0], {}
 
 
-def is_stock_empty(total_count_in_stock):
-    return total_count_in_stock == 0
+def is_stock_empty(total_in_stock):
+    return total_in_stock == 0
 
 
-def is_stock_within_range(total_count_in_stock, lower_bound, upper_bound):
-    return total_count_in_stock >= lower_bound and total_count_in_stock <= upper_bound
+def is_stock_within_range(total_in_stock, lower_bound, upper_bound):
+    return total_in_stock >= lower_bound and total_in_stock <= upper_bound
+
+
+def get_total_in_stock(current_stock, consecutive_failures):
+    if not len(current_stock):
+        handle_and_log_error(consecutive_failures,
+                             'Current stock was not found on the page',
+                             'Failed to find current stock %s consecutive times.' % redify(MAX_RETRIES))
+        return FAILUIRE
+
+    # Attempt to parse the current item count to an integer
+    total_in_stock = 0
+    try:
+        total_in_stock = int(current_stock[0].text)
+    except Exception as e:
+        handle_and_log_error(consecutive_failures,
+                             'Could not parse stock count: \n%s' % e,
+                             'Failed to parse stock count %s consecutive times.' % redify(MAX_RETRIES))
+        return FAILUIRE
+
+    return total_in_stock
 
 
 # Run the script
